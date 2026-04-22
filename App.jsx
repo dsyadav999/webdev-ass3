32bd8f6c841e9a537abd1bb4290f35738e1735ac
dsyadav999 <officialdsyadav999@gmail.com>
2026-04-22 21:43:51 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import StudentTable from "./components/StudentTable";
+import AddStudentForm from "./components/AddStudentForm";
+import "./App.css";
+
+function App() {
+  const [students, setStudents] = useState([
+    { id: 1, name: "Aman", score: 55 },
+    { id: 2, name: "Riya", score: 35 },
+  ]);
+
+  const updateScore = (id, newScore) => {
+    setStudents(students.map(s =>
+      s.id === id ? { ...s, score: Number(newScore) } : s
+    ));
+  };
+
+  const addStudent = (name, score) => {
+    const newStudent = {
+      id: Date.now(),
+      name,
+      score: Number(score)
+    };
+    setStudents([...students, newStudent]);
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      <AddStudentForm addStudent={addStudent} />
+      <StudentTable students={students} updateScore={updateScore} />
+    </div>
+  );
+}
+
+export default App;
